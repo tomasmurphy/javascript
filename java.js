c09e94aa180aa5ea60ef836bc3edb425f85f8fdf
Tomás Murphy <tomas.murphy.tm@gmail.com>
2022-05-17 12:08:30 -0300

    Javascript

---
+++ java.js
@@ -0,0 +1,11 @@
+alert ("hola");
+document.write ("hola mundo"+"<br>")
+
+function sumar(a,b){
+
+    var a = a;
+    var b = b;
+   suma = a + b;
+   document.write("La suma es: "+ suma);
+ }
+ sumar(8,105.5)
