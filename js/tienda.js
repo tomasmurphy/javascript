64c95e345a3232c76eb3de63e5304b08897ee3d0
Tomás Murphy <tomas.murphy.tm@gmail.com>
2022-07-13 12:49:25 -0300

    Prueba final

--- js/tienda.js
+++ js/tienda.js
@@ -8,8 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // VARIABLES 
     const btnVaciar = document.querySelector('#btnVaciar');
-    const btnComprar = document.querySelector('#btnComprar');
-    const btnTodos = document.querySelector("#btnTodos");    
+    const btnComprar = document.querySelector('#btnComprar');   
     const btnEsculturas = document.querySelector("#btnEsculturas");
     const btnPinturas = document.querySelector("#btnPinturas");
     const btnFotos = document.querySelector("#btnFotos");
@@ -18,24 +17,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const divCarrito = document.querySelector('#carrito');
     const total = document.querySelector('#total');
     const miLocalStorage = window.localStorage;
+    const abrirCarrito = document.querySelector("#abrirCarrito");
+    const btnCarrito = document.querySelector("#btnCarrito");
+    const btnBurger = document.querySelector("#navbarNavAltMarkup");
+    const btnInfo = document.querySelector("#btnInfo");
 
     // EVENTOS BOTONES 
+    abrirCarrito.addEventListener("click", () => {
+        abrirCarrito.classList.remove("show");
+    });
+    btnCarrito.addEventListener("click", () => {
+        const prueba = abrirCarrito.classList.contains("show");
+        if(prueba === true){
+        abrirCarrito.classList.remove("show");}else{
+        abrirCarrito.classList.add("show")
+        }
+    });    
     btnEsculturas.addEventListener("click", () => {
-        cargarProductos("Esculturas")
+        cargarProductos("Esculturas");
+        btnBurger.setAttribute("aria-expanded", "false");
+        btnBurger.classList.remove("show");
     });
     btnPinturas.addEventListener("click", () => {
-        cargarProductos("Pinturas")
+        cargarProductos("Pinturas");
+        btnBurger.setAttribute("aria-expanded", "false");
+        btnBurger.classList.remove("show");
     });
     btnFotos.addEventListener("click", () => {
-        cargarProductos("Fotos")
+        cargarProductos("Fotos");
+        btnBurger.setAttribute("aria-expanded", "false");
+        btnBurger.classList.remove("show");
     });
     btnComprar.addEventListener("click", () => {
         Swal.fire({
             title: "Finalizar compra",
-            text: "Comprueba que tus productos son correctos",
+            text: "",
             showCancelButton: true,
             confirmButtonText: "Comprar",
             cancelButtonText: "Cancelar",
+        }).then((result) => {
+            if(result.isConfirmed){
+                Swal.fire({
+                    title: "Tu compra fue realizada con éxito.",
+                    text: "Mentira, es un simulacro",
+                });
+                vaciarCarrito();
+            }
         });
     });
     btnVaciar.addEventListener("click", () => {
@@ -51,6 +78,10 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
     });
+    function traerId(evento){
+        const id = evento.target.dataset.id;
+        cargarProductos(id);
+    };
 
     // CREAR MAIN DONDE CARGAR PRODUCTOS 
     const crearMain = document.createElement('main');
@@ -71,21 +102,34 @@ document.addEventListener('DOMContentLoaded', () => {
         switch (filtro) {
             case "Esculturas":
                 var productosFiltrados = productos.filter(a => a.categoria === filtro);
+                btnEsculturas.classList.remove("activo");
+                btnPinturas.classList.add("activo");
+                btnFotos.classList.add("activo");
                 break;
             case "Pinturas":
                 var productosFiltrados = productos.filter(a => a.categoria === filtro);
+                btnPinturas.classList.remove("activo");
+                btnEsculturas.classList.add("activo");
+                btnFotos.classList.add("activo");
                 break;
             case "Fotos":
                 var productosFiltrados = productos.filter(a => a.categoria === filtro);
+                btnFotos.classList.remove("activo");
+                btnPinturas.classList.add("activo");
+                btnEsculturas.classList.add("activo");
                 break;
-            case "Todos":
-                    var productosFiltrados = productos;
-                    break;
             default:
-                var productosFiltrados = productos
-                break;
+            var productosFiltrados = productos.filter(a => a.id == filtro);
+                btnFotos.classList.add("activo");
+                btnEsculturas.classList.add("activo");
+                btnPinturas.classList.add("activo");
+            
+              break;
         }
+        console.log(productosFiltrados)
 
+        if(productosFiltrados.length > 1){
+        // POR CATEGORIA 
         productosFiltrados.forEach((info) => {
             const divProductos = document.createElement('div');
             divProductos.classList.add('card', 'col-12', 'col-sm-6', 'col-md-3');
@@ -97,9 +141,12 @@ document.addEventListener('DOMContentLoaded', () => {
             const divProductosImagen = document.createElement('img');
             divProductosImagen.classList.add('img-fluid');
             divProductosImagen.setAttribute('src', info.imagen);
-            const divProductosDescripcion = document.createElement('p');
-            divProductosDescripcion.classList.add('card-text');
-            divProductosDescripcion.textContent = `${info.descripcion}`;
+            const divProductosDescripcion = document.createElement('button');
+            divProductosDescripcion.classList.add('btnInfo');
+            divProductosDescripcion.setAttribute("id",'btnInfo');
+            divProductosDescripcion.textContent = `+`;
+            divProductosDescripcion.dataset.id = info.id;
+            divProductosDescripcion.addEventListener('click', traerId);
             const divProductosPrecio = document.createElement('p');
             divProductosPrecio.classList.add('card-text');
             divProductosPrecio.textContent = `$ ${info.precio}`;
@@ -119,13 +166,50 @@ document.addEventListener('DOMContentLoaded', () => {
             crearMain.appendChild(divProductos);
 
         });
+    }else{
+                // POR ID 
+                productosFiltrados.forEach((info) => {
+                    const divProductos = document.createElement('div');
+                    divProductos.classList.add('card', 'col-12');
+                    const divProductosCardBody = document.createElement('div');
+                    divProductosCardBody.classList.add('card-body', "row");
+                    const divProductosTitle = document.createElement('h5');
+                    divProductosTitle.classList.add('card-title');
+                    divProductosTitle.textContent = info.nombre;
+                    const divProductosImagen = document.createElement('img');
+                    divProductosImagen.classList.add('img-fluid', "col-12", "col-md-4");
+                    divProductosImagen.setAttribute('src', info.imagen);
+                    const divProductosDescripcion = document.createElement('p');
+                    divProductosDescripcion.classList.add('card-text', "col-12", "col-md-5");
+                    divProductosDescripcion.textContent = info.descripcion;
+                    const divProductosPrecio = document.createElement('p');
+                    divProductosPrecio.classList.add('card-text');
+                    divProductosPrecio.textContent = `$ ${info.precio}`;
+                    const divProductosBoton = document.createElement('button');
+                    divProductosBoton.classList.add('btn');
+                    divProductosBoton.textContent = 'Agregar al carrito';
+                    divProductosBoton.setAttribute('marcador', info.id);
+                    divProductosBoton.addEventListener('click', agregarAlCarrito);
+        
+                    
+                    divProductosCardBody.appendChild(divProductosTitle);
+                    divProductosCardBody.appendChild(divProductosImagen);
+                    divProductosCardBody.appendChild(divProductosDescripcion);
+                    divProductosCardBody.appendChild(divProductosPrecio);
+                    divProductosCardBody.appendChild(divProductosBoton);
+                    divProductos.appendChild(divProductosCardBody);
+                    crearMain.appendChild(divProductos);
+        
+                });
+            }
     }
 
     function agregarAlCarrito(evento) {
         carrito.push(evento.target.getAttribute('marcador'))
         actualizarCarrito();
         guardarCarritoEnLocalStorage();
-    }
+        abrirCarrito.classList.add("show");
+        }
 
     function actualizarCarrito() {
         divCarrito.textContent = '';
@@ -141,7 +225,7 @@ document.addEventListener('DOMContentLoaded', () => {
             divProductos.classList.add('list-group-item', 'text-right', 'mx-2');
             divProductos.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - $ ${miItem[0].precio}`;
             const miBoton = document.createElement('button');
-            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.classList.add('btn');
             miBoton.textContent = 'X';
             miBoton.style.marginLeft = '1rem';
             miBoton.dataset.item = item;
